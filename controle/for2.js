4d0e87ff5ed091fd5f0561d9d50db2ac4f301295
Gabi012 <gkboves@gmail.com>
2021-03-14 10:52:55 -0300

    Add files via upload

---
+++ controle/for2.js
@@ -0,0 +1,13 @@
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for(i in notas){
+    console.log(i, notas[i])
+}
+const pessoa ={
+    nome: 'Ana',
+    idade: 64,
+    peso:65
+}
+for(let atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`)
+}
